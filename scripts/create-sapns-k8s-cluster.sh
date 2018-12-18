70fa2d6a4ee5a58d0600db56c17e909da992f09a
Michael (Mike) Williams <34947926+mpw07458@users.noreply.github.com>
2018-12-17 21:34:39 -0500

    Update create-sapns-k8s-cluster.sh

--- scripts/create-sapns-k8s-cluster.sh
+++ scripts/create-sapns-k8s-cluster.sh
@@ -1,18 +1,1 @@
-#!/usr/bin/env bash
-kops create cluster \
---node-count 6 \
---admin-access '10.0.0.0/8' \
---ssh-access '10.0.0.0/8' \
---zones us-gov-west-1a,us-gov-west-1b \
---master-zones us-gov-west-1a \
---ssh-public-key ~/.ssh/sbx2-key.pub \
---node-size t2.xlarge \
---master-size m3.medium \
---associate-public-ip=false \
---api-loadbalancer-type internal \
---network-cidr 172.20.0.0/16 \
---topology private \
---networking canal \
---image ami-799b0518 \
---master-volume-size 128 \
-ns2devcluster1.k8s.local
+
